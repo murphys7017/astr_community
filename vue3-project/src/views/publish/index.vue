a52684882dd0479911300a1a0cd0dd58db9d98af
Murphy <54226881+murphys7017@users.noreply.github.com>
2026-04-15 17:14:25 +0800

    feat: 放宽内容长度限制到 5MB

--- vue3-project/src/views/publish/index.vue
+++ vue3-project/src/views/publish/index.vue
@@ -39,8 +39,8 @@
 
         <div class="input-section">
           <div class="content-input-wrapper">
-            <textarea v-model="form.content" class="content-textarea" placeholder="请输入内容（支持 Markdown）" maxlength="50000"></textarea>
-            <div class="char-count">{{ form.content.length }}/50000</div>
+            <textarea v-model="form.content" class="content-textarea" placeholder="请输入内容（支持 Markdown）" maxlength="5242880"></textarea>
+            <div class="char-count">{{ form.content.length }}/5242880</div>
           </div>
         </div>
 
